220c9642b2edec6f51ac0812cb9975087fd38bd9
wdale <wdale@mit.edu>
2023-12-19 22:40:21 -0500

    Finalize simple web server app

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ func formHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	fmt.Fprintf(w, "POST request successful")
+	fmt.Fprintf(w, "POST request successful\n")
 	name := r.FormValue("name")
 	address := r.FormValue("address")
 
@@ -39,7 +39,7 @@ func main() {
 	http.HandleFunc("/hello", helloHandler)
 
 	fmt.Printf("Starting server at port 8080\n")
-	if err := http.ListenAndServe(":8080", nil) ; err != nil {
+	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)
 	}
 }
